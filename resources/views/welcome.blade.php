92ebd9721472c25fc529db74b6961fca169305ed
Will <will@zen71.com>
2024-10-22 16:42:42 +0100

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Darkside Test</title>
+
+        <!-- Styles -->
+        <link href="css/reset.css" rel="stylesheet">
+        <link href="css/stylesheet.css" rel="stylesheet">
+        <!-- Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet'>
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <h3>Darkside Test</h3>
+        </header>
+        <div class="main">
+            <p>Hello world!</p>
+            <form>
+                <label for="name">Name:</label><br>
+                <input type="text" id="name" name="name"><br>
+                <label for="email">Email:</label><br>
+                <input type="email" id="email" name="email"><br>
+                <label for="phone">Phone:</label><br>
+                <input type="tel" id="phone" name="phone"><br>
+                <label for="address">Address:</label><br>
+                <input type="text" id="address" name="address"><br>
+                <button>Submit</button>
+            </form>
+        </div>
+    </body>
+</html>
