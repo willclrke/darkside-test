abc78edb9831df8ae1b907af1ada9349c4a05113
Will <will@zen71.com>
2024-10-23 19:19:26 +0100

    Converted to use vue.js and vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import axios from 'axios'; // Import axios
+import CustomerForm from './components/CustomerForm.vue';
+
+const app = createApp({});
+app.component('customer-form', CustomerForm);
+app.mount('#app');
+
+// CSRF
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+axios.defaults.headers.common['X-CSRF-TOKEN'] = token;
